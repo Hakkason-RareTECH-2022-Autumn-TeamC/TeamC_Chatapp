d1873e30b80c1e62acb04c52f4b5b24fc2394e08
GoOkumura <87104566+GoOkumura@users.noreply.github.com>
2022-11-09 16:56:58 +0900

    app.py作成&記述:templates/base.html作成&記述:requirements.txt追記

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager
+
+from werkzeug.security import generate_password_hash, check_pasword_hash
+import os
+
+from datetime import datetime
+import pytz
+
+# app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+# app.config["SECRET_KEY"] = os.urandom(24)
+# db = SQLAlchemy(app)
+
+# login_manager = LoginManager()
+# login_manager.init_app()
+
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(30), unique=True, nullable=False)
+#     email = db.Column(db.String(50))
+#     password = db.Column(db.String(30))
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     email = db.Column(db.String)
+
+# @app.route("/")
+# def index():
+#     return render_template("test.html")
