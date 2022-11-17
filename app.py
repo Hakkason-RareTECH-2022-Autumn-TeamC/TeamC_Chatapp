9f5a788b25f501128a7c1880e3b9f8f19f7cb5bd
GoOkumura <87104566+GoOkumura@users.noreply.github.com>
2022-11-17 17:42:26 +0900

    ログイン機能/ユーザ登録機能の実装(途中)

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, LoginManager, login_user, logout_user
-
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
+#パスワードをハッシュ化して登録/チェックする機能をインポート
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
@@ -17,13 +17,14 @@ db = SQLAlchemy(app)
 Login_manager = LoginManager()
 Login_manager.init_app(app)
 
-# class User(UserMixin, db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(30), nullable=False, unique=True)
-#     email = db.Column(db.String(50),nullable=False)
-#     password = db.Column(db.String(12))
-#     mymessageid = db.Column(db.Integer, db.ForeignKey("MyMessage.id"))
-#     relasionid = db.Column(db.Integer, db.ForeignKey("Relation.id"))
+# 1.Userテーブルを定義
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(50),nullable=False, unique=True)
+    password = db.Column(db.String(12))
+    mymessageid = db.Column(db.Integer, db.ForeignKey("MyMessage.id"))
+    relasionid = db.Column(db.Integer, db.ForeignKey("Relation.id"))
 
 # class Message(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -41,31 +42,47 @@ Login_manager.init_app(app)
 
 
 
+@Login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 
 
 # -------------------------------- 以下にロジックを記述する ---------------------------------
 
-#【2-1】 : 作業中☆ ユーザログイン画面 担当：奥村
-@app.route("/login", methods=["GET", "POST"])
+#【2-1】 : 作業中☆ ユーザログイン機能 担当：奥村
+@app.route("/signin", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
 
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(email=email).first()
         if check_password_hash(user.password, password):
             login_user(user)
             return redirect("/usertop")
     else:
         return render_template("test_oku.html")
 
+# #【2-2】 : 新規ユーザ登録画面
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email= request.form.get("email")
+        password = request.form.get("password")
+
+        user = User(username=username, email=email, password=generate_password_hash(password, method="sha256"))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/signin")
+
+    else:
+        return render_template("test_signup.html")
 
 
-
-
-
-#【2-2】 : ユーザ登録画面
 #【2-3】 : パスワード再設定
 # ☆優先！【3-1】: ユーザTOP画面 担当：ibu
 
@@ -79,31 +96,6 @@ def login():
 
 #【3-3】: ユーザ情報変更
 #【3-4】: 新規つながり検索
+
+
 # ☆優先！【4-1】: チャットルーム
-
-
-
-
-
-
-
-
-# ---------------------------- 以下は教材youtubeで記述された模範コード（転用時は注意）----------------------------
-
-# login_manager = LoginManager()
-# login_manager.init_app()
-
-# class User(UserMixin, db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(30), unique=True, nullable=False)
-#     email = db.Column(db.String(50))
-#     password = db.Column(db.String(30))
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String, unique=True, nullable=False)
-#     email = db.Column(db.String)
-
-# @app.route("/")
-# def index():
-#     return render_template("test.html")
